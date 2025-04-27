5da28bb4e18cbed6c663601ccf02484afefbb4d5
justkashish <meghanikashish1@gmail.com>
2025-04-27 16:42:01 +0530

    added backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://journal-server-j0r5.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
